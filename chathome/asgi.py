4a12eeae47f8263ab4efa2c26f4e1e93d578709f
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-02-02 02:03:26 +0100

    deply

--- chathome/asgi.py
+++ chathome/asgi.py
@@ -7,18 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os, django
+import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chathome.settings")
+django_asgi_app = get_asgi_application()
+
 from chat_control.route import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from chat_control.channels_middleware import JWTWebsocketMiddleware
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chathome.settings")
-django.setup()
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
