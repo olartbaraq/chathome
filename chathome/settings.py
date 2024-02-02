9a13bfafad2578aca4dcff26785e58f32825aa7a
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-02-02 02:25:54 +0100

    deploy

--- chathome/settings.py
+++ chathome/settings.py
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("10.1.91.212", 6379)],
         },
     },
 }
