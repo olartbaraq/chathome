7c60c06ee4e87da75adcd127ce5855b2d73025be
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-02-02 01:53:57 +0100

    deploy

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne chathome.asgi:application --port $PORT --bind 0.0.0.0
+web: daphne chathome.asgi:application --port $PORT --bind 0.0.0.0 -v2
 chatworker: python manage.py runworker --settings=chathome.settings -v2
--- chathome/settings.py
+++ chathome/settings.py
@@ -33,7 +33,6 @@ AUTH_USER_MODEL = "user_control.User"
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,6 +44,8 @@ INSTALLED_APPS = [
     "drf_yasg",
     "corsheaders",
     "chat_control",
+    "daphne",
+    "channels",
 ]
 
 MIDDLEWARE = [
