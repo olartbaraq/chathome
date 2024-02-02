276034f0bbdb1cecde514898e8cbf0fbfb584c36
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-02-02 03:10:20 +0100

    deploy

--- chathome/asgi.py
+++ chathome/asgi.py
@@ -11,12 +11,12 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chat_control.route import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chathome.settings")
 django.setup()
 django_asgi_app = get_asgi_application()
 
+from chat_control.route import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from chat_control.channels_middleware import JWTWebsocketMiddleware
 
